73498ee54d01c450b4a58fca613d808035e11697
Chandrakanth_c <chandrakanthcandikattu@gmail.com>
2018-03-09 09:12:39 +0530

    Eclipse Project files added

---
+++ src/com/servlet3/sample/Sample.java
@@ -0,0 +1,51 @@
+package com.servlet3.sample;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Sample
+ */
+
+@WebServlet("/Sample")
+@WebInitParam(name = "ServletConfigParam", value = "Value1")
+public class Sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rD = request.getRequestDispatcher("/SampleJSP.jsp");
+		request.setAttribute("InitParams", getServletConfig().getInitParameter("ServletConfigParam"));
+		rD.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
